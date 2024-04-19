e2c8c0536386626ac18d12629208056c1cbc39a3
AlexTLG <postalti@mail.ru>
2024-04-20 00:09:20 +0300

    long redirect Ex2_2 done

---
+++ src/test/java/Ex2_2.java
@@ -0,0 +1,24 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import io.restassured.http.Headers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex2_2 {
+    @Test
+    public void long_redirect_1() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader = response.getHeader("location");
+        System.out.println(locationHeader);
+    }
+}

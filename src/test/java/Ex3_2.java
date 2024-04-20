8db2baa34d66a457030e1eeb4a0fe6d9f3ec878e
AlexTLG <postalti@mail.ru>
2024-04-20 18:37:45 +0300

    long redirect_2 Ex3_2 done

--- src/test/java/Ex3_2.java
+++ src/test/java/Ex3_2.java
@@ -19,7 +19,7 @@ public class Ex3_2 {
             Response response = RestAssured
                     .given()
                     .redirects()
-                    .follow(true)
+                    .follow(false)
                     .when()
                     .get(url)
                     .andReturn();
